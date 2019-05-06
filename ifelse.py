9ba130f809603db8c52f53b74765358336b1f65d
m.avsek94 <m.avsek94@gmail.com>
2019-05-06 23:25:54 +0000

    initial commit

---
+++ ifelse.py
@@ -0,0 +1,12 @@
+#!/usr/src/Python-3.7.2/python
+user = { 'admin': True, 'active': '', 'name': 'Abhishek'}
+prefix = ""
+
+if user['active'] and user['admin']:
+    prefix = "ACTIVE - (ADMIN)"
+elif user['admin']:
+    prefix = "(ADMIN)"
+elif user['active']:
+    prefix = "ACTIVE - "
+
+print(prefix + user['name'])
