7fcc5e449b6654c4a9927fe7b865469e2367c553
m.avsek94 <m.avsek94@gmail.com>
2019-05-07 01:27:33 +0000

    user input example

---
+++ userinput.py
@@ -0,0 +1,18 @@
+#!/usr/src/Python-3.7.2/python
+
+from datetime import date
+
+
+name =input('what is your name ?')
+birthyear = input("what is your birth year ?")
+birthmonth = input("what is your birth month ?")
+birthday = input("what is your birth day ?")
+age = input("How Old are you rth?")
+birthdate = input("Enter your date of birth ?")
+print(f"{name} was born in {birthdate}")
+
+
+
+
+
+
